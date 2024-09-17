78f397ff1bcf0b6f3574eb052e2426e7e26edc55
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-17 17:58:27 -0400

    feat: rework linking to have strong refs on child, not parent

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,8 @@ struct InMemoryNode {
     metadata: NodeMetadata,
 
     // Tree data structure refs:
-    parent: Option<Rc<RefCell<InMemoryNode>>>,
-    children: Vec<Weak<RefCell<InMemoryNode>>>,
+    parent: Option<Weak<RefCell<InMemoryNode>>>,
+    children: Vec<Rc<RefCell<InMemoryNode>>>,
 
     // Linked list data structure refs:
     next: Option<Weak<RefCell<InMemoryNode>>>,
@@ -42,25 +42,43 @@ impl InMemoryNode {
         }))
     }
 
-    fn dump(node: Rc<RefCell<Self>>) {
-        Self::dump_child( &Rc::downgrade(&node), "");
+    fn dump(node: &Rc<RefCell<Self>>) {
+        Self::dump_child(node, "", None);
     }
-    fn dump_child(node: &Weak<RefCell<Self>>, spacer: &str) {
-        if let Some(upgraded) = node.upgrade() {
-            let borrowed = upgraded.borrow();
+    fn dump_child(node: &Rc<RefCell<Self>>, spacer: &str, parent_expected_index: Option<usize>) {
+        let borrowed = node.borrow();
 
-            println!(
-                "{spacer}- metadata={:?} parent={:?}",
-                borrowed.metadata,
-                borrowed.parent,
-            );
-            if borrowed.metadata == NodeMetadata::Empty && borrowed.children.is_empty() {
-                println!("{spacer}  (no children)")
-            } else {
-                let new_spacer = &format!("{spacer}  ");
-                for child in &borrowed.children {
-                    Self::dump_child(child, new_spacer);
+        // Check to see if in the parent of the given child, there exists a child at the given
+        // index with matching metadata
+        let found_at_index_in_parent = if let (Some(parent_expected_index), Some(parent)) = (parent_expected_index, borrowed.parent.clone()) {
+            parent.upgrade().map(|parent| {
+                let borrowed_parent = parent.borrow();
+                let child = borrowed_parent.children.get(parent_expected_index);
+                if let Some(child) = child {
+                    // A child can be found at the expected index in the parent
+                    child.borrow().metadata == borrowed.metadata
+                } else {
+                    // No child can be found at the expected index in the parent
+                    false
                 }
+            })
+        } else { None };
+
+        // borrowed.parent.children.contains(borrowed)
+        println!(
+            "{spacer}{}. metadata={:?} {}",
+            if let Some(index) = parent_expected_index { format!("{index}") } else { "0".into() },
+            borrowed.metadata,
+            if let Some(result) = found_at_index_in_parent { format!("parent_linked_right?={result}") } else { "".into() }
+        );
+        if borrowed.metadata == NodeMetadata::Empty && borrowed.children.is_empty() {
+            println!("{spacer}  (no children)")
+        } else {
+            let new_spacer = &format!("{spacer}  ");
+            let mut counter = 0;
+            for child in &borrowed.children {
+                Self::dump_child(child, new_spacer, Some(counter));
+                counter += 1;
             }
         }
     }
@@ -68,21 +86,21 @@ impl InMemoryNode {
     fn append_child(
         parent: Rc<RefCell<Self>>,
         child: Rc<RefCell<Self>>,
-    ) -> (Rc<RefCell<Self>>, Rc<RefCell<Self>>) {
+    ) -> Rc<RefCell<Self>> {
         {
             let mut child_mut = child.borrow_mut();
 
             // Step 1: Add parent as `child.parent`
-            (*child_mut).parent = Some(parent.clone());
+            (*child_mut).parent = Some(Rc::downgrade(&parent));
         }
 
         {
             let mut parent_mut = parent.borrow_mut();
             // Step 2: Add child into `parent.children`
-            (*parent_mut).children.push(Rc::downgrade(&child));
+            (*parent_mut).children.push(child.clone());
         }
 
-        (parent, child)
+        parent
     }
 }
 
@@ -91,8 +109,8 @@ fn main() {
     let foo = InMemoryNode::new_from_literal("foo");
     let bar = InMemoryNode::new_from_literal("bar");
 
-    let (parent, _child) = InMemoryNode::append_child(parent, foo);
-    let (parent, _child) = InMemoryNode::append_child(parent, bar);
+    let parent = InMemoryNode::append_child(parent, foo);
+    let parent = InMemoryNode::append_child(parent, bar);
 
-    InMemoryNode::dump(parent);
+    InMemoryNode::dump(&parent);
 }
