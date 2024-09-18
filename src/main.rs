f8c54bbad92e78be39d01c44c6738c2b4357fbea
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-17 23:07:30 -0400

    feat: add more nodes to demo

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,12 @@ fn main() {
     let foo = InMemoryNode::new_from_literal("foo");
     let bar = InMemoryNode::new_from_literal("bar");
     let baz = InMemoryNode::new_from_literal("baz");
+    let quux = InMemoryNode::new_from_literal("quux");
+    let hello = InMemoryNode::new_from_literal("hello");
 
     let foo = InMemoryNode::append_child(foo, baz);
+    let foo = InMemoryNode::append_child(foo, quux);
+    let foo = InMemoryNode::append_child(foo, hello);
     let parent = InMemoryNode::append_child(parent, foo);
     let parent = InMemoryNode::append_child(parent, bar);
 
