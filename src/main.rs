ef4bd666170a159fcc88e84259dfa17e8fd2299b
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-22 18:32:17 -0400

    feat: add another test

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,22 @@ fn main() {
     let hello = InMemoryNode::new_from_literal("hello");
     let world = InMemoryNode::new_from_literal("world");
 
+    // Test 1:
+    // let foo = InMemoryNode::append_child(foo, baz);
+    // let foo = InMemoryNode::append_child(foo, quux);
+    // let hello = InMemoryNode::append_child(hello, world);
+    // let foo = InMemoryNode::append_child(foo, hello);
+    // let parent = InMemoryNode::append_child(parent, foo);
+    // let parent = InMemoryNode::append_child(parent, bar);
+
+    // Test 2:
+    let foo = InMemoryNode::append_child(foo, bar);
     let foo = InMemoryNode::append_child(foo, baz);
-    let foo = InMemoryNode::append_child(foo, quux);
-    let hello = InMemoryNode::append_child(hello, world);
-    let foo = InMemoryNode::append_child(foo, hello);
+    let quux = InMemoryNode::append_child(quux, hello);
+    let quux = InMemoryNode::append_child(quux, world);
+
     let parent = InMemoryNode::append_child(parent, foo);
-    let parent = InMemoryNode::append_child(parent, bar);
+    let parent = InMemoryNode::append_child(parent, quux);
 
     println!("");
     InMemoryNode::dump(&parent);
