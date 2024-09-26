fef957f4797a000a976196cb77e8ee7b24d66bc5
Ryan Gaus <rsg1egoman@gmail.com>
2024-09-25 20:29:00 -0400

    feat: add more complex test code in fn main()

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod node_tree;
 
 use crate::node_tree::{
     cursor::{Cursor, CursorSeek},
-    node::InMemoryNode,
+    node::{InMemoryNode, NodeSeek},
     utils::Inclusivity,
 };
 
@@ -18,13 +18,13 @@ fn main() {
     // println!("");
     // InMemoryNode::dump(&foo);
 
-    let parent = InMemoryNode::new_empty();
-    let foo = InMemoryNode::new_from_literal("foo");
-    let bar = InMemoryNode::new_from_literal("bar");
-    let baz = InMemoryNode::new_from_literal("baz ");
-    let quux = InMemoryNode::new_from_literal("quux");
-    let hello = InMemoryNode::new_from_literal("hello");
-    let world = InMemoryNode::new_from_literal("world");
+    // let parent = InMemoryNode::new_empty();
+    // let foo = InMemoryNode::new_from_literal("foo");
+    // let bar = InMemoryNode::new_from_literal("bar");
+    // let baz = InMemoryNode::new_from_literal("baz ");
+    // let quux = InMemoryNode::new_from_literal("quux");
+    // let hello = InMemoryNode::new_from_literal("hello");
+    // let world = InMemoryNode::new_from_literal("world");
 
     // // Test 1:
     // // let foo = InMemoryNode::append_child(foo, baz);
@@ -34,17 +34,17 @@ fn main() {
     // // let parent = InMemoryNode::append_child(parent, foo);
     // // let parent = InMemoryNode::append_child(parent, bar);
 
-    // Test 2:
-    InMemoryNode::append_child(&foo, bar);
-    InMemoryNode::append_child(&foo, baz);
-    InMemoryNode::append_child(&quux, hello);
-    InMemoryNode::append_child(&quux, world);
+    // // Test 2:
+    // InMemoryNode::append_child(&foo, bar);
+    // InMemoryNode::append_child(&foo, baz);
+    // InMemoryNode::append_child(&quux, hello);
+    // InMemoryNode::append_child(&quux, world);
 
-    InMemoryNode::append_child(&parent, foo);
-    InMemoryNode::append_child(&parent, quux.clone());
+    // InMemoryNode::append_child(&parent, foo);
+    // InMemoryNode::append_child(&parent, quux.clone());
 
-    println!("");
-    InMemoryNode::dump(&parent);
+    // println!("");
+    // InMemoryNode::dump(&parent);
 
     // // Remove test:
     // println!("");
@@ -57,7 +57,13 @@ fn main() {
     // println!("");
     // InMemoryNode::dump(&parent);
 
-    // let results = InMemoryNode::seek_forwards_until(&parent, |_node, _ct| SeekResult::Continue);
+    // let results = InMemoryNode::seek_forwards_until(&parent, |node, _ct| NodeSeek::Continue(InMemoryNode::literal(node)));
+    // println!("RESULT: {:?}", results);
+
+    // let results = InMemoryNode::seek_backwards_until(&quux, |node, _ct| NodeSeek::Continue(InMemoryNode::literal(node)));
+    // println!("RESULT: {:?}", results);
+
+    // let results = InMemoryNode::seek_forwards_until(&parent, |_node, _ct| NodeSeek::Continue);
     // let results = InMemoryNode::seek_forwards_until(&parent, |node, ct| {
     //     if ct < 3 {
     //         NodeSeek::Continue(node.clone())
@@ -69,7 +75,6 @@ fn main() {
     // let string = results.fold("".into(), |acc, node| format!("{acc} {:?}", node.borrow().metadata));
     // println!("STRING: {:?}", string);
 
-    let cur = Cursor::new(parent);
     // let output = cur.seek_forwards_until(|_character, ct| {
     //     if ct < 5 {
     //         CursorSeek::Continue
@@ -80,13 +85,36 @@ fn main() {
     //     }
     // });
     // let output = cur.seek_forwards(CursorSeek::AdvanceByCharCount(5));
-    // let output = cur.seek_forwards(CursorSeek::AdvanceUntil(Rc::new(|c| {
+    // let output = cur.seek_forwards(CursorSeek::advance_until(|c| {
     //     if c == 'w' { CursorSeekAdvanceUntil::Stop } else { CursorSeekAdvanceUntil::Continue }
-    // })));
+    // }));
     // let output = cur.seek_forwards(CursorSeek::advance_until_char_then_stop(' '));
     // let output = cur.seek_forwards(CursorSeek::advance_until_char_then_done(' '));
-    let (cur, output) = cur.seek_forwards(CursorSeek::advance_upper_word(Inclusivity::Exclusive));
+    // let (cur, output) = cur.seek_forwards(CursorSeek::advance_upper_word(Inclusivity::Exclusive));
     // let output = cur.seek_forwards(CursorSeek::advance_lower_word(CursorInclusivity::Exclusive));
     // let output = cur.seek_forwards(CursorSeek::advance_upper_word(CursorInclusivity::Inclusive));
-    println!("STRING: {:?}", output);
+    // let (cur, output) = cur.seek_backwards_until(|_character, ct| {
+    //     if ct < 3 {
+    //         CursorSeek::Continue
+    //     } else if ct == 3 {
+    //         CursorSeek::AdvanceByCharCount(2)
+    //     } else {
+    //         CursorSeek::Done
+    //     }
+    // });
+
+    let parent = InMemoryNode::new_tree_from_literal_in_chunks("foo:bar baz hello world", 4);
+    InMemoryNode::dump(&parent);
+
+    let cur = Cursor::new_at(parent, 0);
+    // let cur = Cursor::new(parent);
+    // let (cur, output) = cur.seek_forwards(CursorSeek::AdvanceByCharCount(10));
+    // println!("FORWARDS: {:?} {:?}\n", cur, output);
+    let inclusivity = Inclusivity::Inclusive;
+    let (cur, output) = cur.seek_forwards(CursorSeek::advance_lower_word(inclusivity));
+    println!("FORWARDS: {:?} {:?}", cur, output);
+    let (cur, output) = cur.seek_backwards(CursorSeek::advance_lower_word(inclusivity));
+    // let (cur, output) = cur.seek_backwards(CursorSeek::AdvanceByCharCount(5));
+    println!("BACKWARDS: {:?} {:?}", cur, output);
+    // foobarbaz quuxhelloworld
 }
