a99b718c756672f3516dc6141cd480d8da1c73ce
Ryan Gaus <rsg1egoman@gmail.com>
2024-11-03 10:50:01 -0500

    feat: add example raw language definition

---
+++ src/languages/raw.rs
@@ -0,0 +1,30 @@
+use std::{rc::Rc, cell::RefCell};
+use colored::ColoredString;
+
+use crate::node_tree::node::{TokenKindTrait, InMemoryNode};
+
+/// The size of characters from the literal stored in each ast node.
+const RAW_LITERAL_NODE_CHUNK_SIZE_CHARS: usize = 32;
+
+/// The Raw language definition performs no syntax parsing at all, and purely treats the whole text
+/// document as unparsable text.
+#[derive(Debug, Clone, PartialEq)]
+pub enum SyntaxKind {}
+
+impl TokenKindTrait for SyntaxKind {
+    fn apply_debug_syntax_color(text: String, _ancestry: std::vec::IntoIter<SyntaxKind>) -> ColoredString {
+        text.into()
+    }
+
+    // Any node can be reparsed, since a reparse is effectively a noop at the moment
+    fn is_reparsable(&self) -> bool {
+        true
+    }
+
+    fn parse(literal: &str, _parent: Option<Rc<RefCell<InMemoryNode<Self>>>>) -> Rc<RefCell<InMemoryNode<Self>>> {
+        InMemoryNode::new_tree_from_literal_in_chunks(
+            literal,
+            RAW_LITERAL_NODE_CHUNK_SIZE_CHARS,
+        )
+    }
+}
